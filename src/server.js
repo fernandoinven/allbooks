88f6d95841c9dec6f4fb10ef3cb8cd136b0636e7
Fernando Invencao <fernandoinven@hotmail.com>
2026-02-14 01:43:18 -0300

    Altera o server

--- src/server.js
+++ src/server.js
@@ -18,9 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Acesse essa API em http://localhost:8000")
-=======
-  console.log("API disponível em http://localhost:8000")
->>>>>>> b9d7dcc (adiciona projeto pelo Mac)
+console.log("API disponível em http://localhost:8000")
 })
